af4ca82d2a1f03839cac97b94fb1a30bca62b538
Maciej Wysocki <maciej.wysocki@gmail.com>
2017-01-11 22:36:48 +0100

    Adding white marker and judgement descriptions.

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,6 +1,6 @@
-var colors = ['#000', '#1e8dcb', '#c70d1a', '#f0eef1'];
+let colors = ['#000', '#1e8dcb', '#c70d1a', '#f0eef1'];
 
-var chart = c3.generate({
+let chart = c3.generate({
     bindto: '#chart',
     data: {
         columns: [
@@ -34,36 +34,38 @@ var chart = c3.generate({
     }
 });
 
-var sketcher = atrament('#canvas', 400, 400);
+let sketcher = atrament('#canvas', 400, 400);
 sketcher.weight = 20;
 sketcher.smoothing = false;
 
-var tool = document.getElementById("tool-marker-black");
+let tool = document.getElementById("tool-marker-black");
 tool.addEventListener("click", function () {
     sketcher.color = "#000000";
-    // sketcher.mode = "draw";
+    sketcher.mode = "draw";
 });
-var tool = document.getElementById("tool-marker-blue");
+tool = document.getElementById("tool-marker-blue");
 tool.addEventListener("click", function () {
     sketcher.color = "#0000ff";
-    // sketcher.mode = "draw";
+    sketcher.mode = "draw";
 });
-var tool = document.getElementById("tool-eraser");
+tool = document.getElementById("tool-marker-white");
+tool.addEventListener("click", function () {
+    sketcher.mode = "erase";
+});
+tool = document.getElementById("tool-eraser");
 tool.addEventListener("click", function () {
     sketcher.clear();
     drawing = true;
     timeoutFunction();
-    // sketcher.mode = "erase";
 });
 
-var canvas = document.getElementById("canvas");
-var drawing = false;
-var timeout;
+let canvas = document.getElementById("canvas");
+let drawing = false;
+let timeout;
 
-var timeoutFunction = function () {
+let timeoutFunction = function () {
     if (drawing) {
         timeout = setTimeout(function () {
-            // alert(canvas.toDataURL());
             if(drawing) {
                 sendImage();
                 drawing = false;
@@ -79,27 +81,45 @@ canvas.addEventListener("mousedown", function () {
     clearTimeout(timeout);
 });
 
-var sendImage = function () {
-    var request = new XMLHttpRequest();
+let sendImage = function () {
+    let request = new XMLHttpRequest();
     request.open('POST', '/recognize', true);
 
     request.onload = function () {
         if (this.status >= 200 && this.status < 400) {
-            // Success!
-            let data = JSON.parse(this.response);
-            // chart.unload();
-            chart.load({
-                columns: [['data', 100*data.data[0], 100*data.data[1], 100*data.data[2], 100*data.data[3]]]
-            });
-        } else {
-            // We reached our target server, but it returned an error
-
-        }
-    };
-
-    request.onerror = function () {
-        // There was a connection error of some sort
+            updateJudgement(JSON.parse(this.response).data);
+        } 
     };
 
     request.send(canvas.toDataURL());
+};
+
+let carMap = ['BMW', 'Ford', 'Mazda', 'Mercedes'];
+
+let updateJudgement = function(data) {
+    chart.load({
+        columns: [['data', 100*data[0], 100*data[1], 100*data[2], 100*data[3]]]
+    });
+
+    let description = "I am not sure what is it.";
+
+    let maxData = 0;
+    let maxIndex = -1;
+    for(let i = 0; i < data.length; ++i) {
+        if(data[i] > maxData) {
+            maxData = data[i];
+            maxIndex = i;
+            if(maxData > 0.7) {
+                description = "I am sure it is ";
+            } else if (maxData > 0.5) {
+                description = "I think it is ";
+            } else if (maxData > 0.3) {
+                description = "It mimght be ";
+            }
+        }
+    }
+    if(maxData > 0.3) {
+        description += carMap[maxIndex];
+    }
+    document.getElementById('answer').innerText = description;
 };
